e8daaccedb759c8e61cf3315e7e0c2136ec6eac3
Vinit Kanse <vinitkansend@gmail.com>
2022-02-03 23:32:47 +0530

    Feat: Added rating

---
+++ components/Rating/rating.js
@@ -0,0 +1,33 @@
+const ratingStars = document.querySelectorAll(".rating input");
+const form = document.querySelector(".submit-review");
+const showReview = document.querySelector(".show-review");
+
+function checkStars(idx) {
+  if (ratingStars[idx].checked) {
+    for (let j = idx - 1; j >= 0; j--) {
+      ratingStars[j].checked = true;
+    }
+  } else {
+    for (let j = idx + 1; j < ratingStars.length; j++) {
+      ratingStars[j].checked = false;
+    }
+  }
+}
+
+export function rating() {
+  ratingStars.forEach((star, id) => {
+    star.addEventListener("change", () => checkStars(id));
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let sum = 0;
+    ratingStars.forEach((star) => {
+      if (star.checked) sum += 1;
+    });
+
+    if (sum != 0) showReview.innerText = `Thank you for ${sum} stars!`;
+  });
+}
+
+rating();
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { modal } from "./components/Modal/modal";
 import { toast } from "./components/Toast/toast";
+import { rating } from "./components/Rating/rating";
 
 // modal();
 // toast();
+// rating()
