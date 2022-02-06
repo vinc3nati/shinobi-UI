00a12e6a547fcc54e5f2e01ebbe925f6aff3999b
Vinit Kanse <vinitkansend@gmail.com>
2022-02-06 23:35:59 +0530

    Feat: completed landing and avatar page

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ import { rating } from "./components/Rating/rating";
 modal();
 toast();
 rating();
+
+window.addEventListener("scroll", () => {
+  let nav = document.querySelector(".navbar");
+  let windowPosition = window.scrollY > 100;
+  nav.classList.toggle("scrolling-active", windowPosition);
+});
