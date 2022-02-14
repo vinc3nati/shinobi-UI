ade54f3763fb7ccda9565d57b07314d276cdcf88
Vinit Kanse <vinitkansend@gmail.com>
2022-02-14 23:59:33 +0530

    Feat: Added Side Nav for responsiveness

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import { rating } from "./components/Rating/rating";
 
 const btnToTop = document.querySelector("[data-top]");
 const rootElement = document.documentElement;
+const drawerBtn = document.querySelector("[data-drawer]");
+const drawer = document.querySelector(".drawer");
+const backdrop = document.querySelector(".backdrop");
 
 modal();
 toast();
@@ -26,4 +29,16 @@ btnToTop.addEventListener("click", () => {
   rootElement.scrollTo({
     top: 0,
   });
+});
+
+if (drawerBtn) {
+  drawerBtn.addEventListener("click", () => {
+    backdrop.style.display = "block";
+    drawer.style.transform = "translateX(0)";
+  });
+}
+
+backdrop.addEventListener("click", () => {
+  backdrop.style.display = "none";
+  drawer.style.transform = "translateX(-100%)";
 });
